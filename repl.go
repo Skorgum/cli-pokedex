454de7368cfd7d1fa541d5c0f3ea8632ca5cabe1
Skorgum <skorgum@gmail.com>
2025-12-08 21:32:35 -0500

    REPL initalize and wire

--- repl.go
+++ repl.go
@@ -1,9 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
 	words := strings.Fields(lower)
 	return words
+}
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
 }
