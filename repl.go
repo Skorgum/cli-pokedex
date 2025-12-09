1bae425351ea3d92aeff899a7e9c0fd4ee754088
Skorgum <skorgum@gmail.com>
2025-12-09 10:56:27 -0500

    Add Exit and Help commands

--- repl.go
+++ repl.go
@@ -22,8 +22,58 @@ func startRepl() {
 		if len(words) == 0 {
 			continue
 		}
+		cmdName := words[0]
+		cmd, ok := commands[cmdName]
+		if !ok {
+			fmt.Printf("Unknown command: %s\n", cmdName)
+			continue
+		}
+		err := cmd.callback()
+		if err != nil {
+			fmt.Printf("Error executing command %s: %s\n", cmdName, err)
+			continue
+		}
+	}
+}
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return nil
+}
 
-		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	order := []string{"help", "exit"}
+
+	for _, name := range order {
+		cmd := commands[name]
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 	}
 }
