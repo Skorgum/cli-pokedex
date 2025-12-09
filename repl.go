2d6fa55fc4fe0398e4e5779ee01e1732a0603f9d
Skorgum <skorgum@gmail.com>
2025-12-09 14:14:24 -0500

    Add and test map and mapb commands

--- repl.go
+++ repl.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
+
+	"github.com/Skorgum/cli-pokedex/internal/pokeapi"
 )
 
 func cleanInput(text string) []string {
@@ -14,6 +17,9 @@ func cleanInput(text string) []string {
 }
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+	cfg := &config{
+		PokeClient: pokeapi.NewClient(5 * time.Second),
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -28,25 +34,25 @@ func startRepl() {
 			fmt.Printf("Unknown command: %s\n", cmdName)
 			continue
 		}
-		err := cmd.callback()
+		err := cmd.callback(cfg)
 		if err != nil {
 			fmt.Printf("Error executing command %s: %s\n", cmdName, err)
 			continue
 		}
 	}
 }
-func commandExit() error {
+func commandExit(cfg *config) error {
 	fmt.Print("Closing the Pokedex... Goodbye!\n")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 
-	order := []string{"help", "exit"}
+	order := []string{"help", "exit", "map", "mapb"}
 
 	for _, name := range order {
 		cmd := commands[name]
@@ -55,10 +61,52 @@ func commandHelp() error {
 	return nil
 }
 
+func commandMap(cfg *config) error {
+	res, err := cfg.PokeClient.ListLocations(cfg.Next)
+	if err != nil {
+		return err
+	}
+
+	for _, location := range res.Results {
+		fmt.Println(location.Name)
+	}
+
+	cfg.Next = res.Next
+	cfg.Previous = res.Previous
+
+	return nil
+}
+
+func commandMapB(cfg *config) error {
+	if cfg.Previous == nil {
+		fmt.Println("You're on the first page")
+		return nil
+	}
+	res, err := cfg.PokeClient.ListLocations(cfg.Previous)
+	if err != nil {
+		return err
+	}
+
+	for _, location := range res.Results {
+		fmt.Println(location.Name)
+	}
+
+	cfg.Next = res.Next
+	cfg.Previous = res.Previous
+
+	return nil
+}
+
+type config struct {
+	Next       *string
+	Previous   *string
+	PokeClient pokeapi.Client
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
 var commands map[string]cliCommand
@@ -74,6 +122,16 @@ func init() {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays 20 locations. Subsequent call displays the next 20, and so on",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 locations",
+			callback:    commandMapB,
 		},
 	}
 }
